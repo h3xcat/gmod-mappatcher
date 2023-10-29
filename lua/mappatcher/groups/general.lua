1db234fbd0d3eb291deb973266970f8ccc4c095d
H3xCat <2304691+h3xcat@users.noreply.github.com>
2023-10-29 04:23:20 -0700

    Fix localization string for superadmin

--- lua/mappatcher/groups/general.lua
+++ lua/mappatcher/groups/general.lua
@@ -1,4 +1,4 @@
 -- These should be rewriten by ulx.lua, if ULX is enabled
 MapPatcher.Groups.Register("everyone", function(ply) return true end, "#mappatcher.groups.general.everyone" )
 MapPatcher.Groups.Register("admin", function(ply) return ply:IsAdmin() end, "#mappatcher.groups.general.admin" )
-MapPatcher.Groups.Register("superadmin", function(ply) return ply:IsSuperAdmin() end, "mappatcher.groups.general.superadmin" )
+MapPatcher.Groups.Register("superadmin", function(ply) return ply:IsSuperAdmin() end, "#mappatcher.groups.general.superadmin" )
